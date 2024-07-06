9a67e5d9489c3a6b7a31acaa2a6256b2c4d4da24
Tarun <tarunsharma396d@gmail.com>
2024-07-06 23:13:58 +0530

    ios auth fix

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.set('trust proxy', 1);
 
 // CORS configuration
 // const corsOptions = {
